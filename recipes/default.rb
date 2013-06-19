40cc29608e45f2e7746dc06c10565951c9344192
Diego Rodriguez <drodriguez@delphi.us>
2013-06-19 14:50:15 -0700

    Updated recipe copyright notice

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright 2009, Opscode, Inc.
+# Copyright 2013, OpenGov
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
