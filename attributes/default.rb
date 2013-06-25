1d1e818e521f9ea2f2213ecaa588c7509151cdfd
Xavier Lange <xrlange@gmail.com>
2013-06-25 14:30:31 -0700

    God now support init and runit

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,12 @@
 default['god']['bin']               = '/usr/bin/god'
-default['god']['email']['from']     = ['god@'+node[:domain]]
-default['god']['email']['contacts'] = {'ops' => 'ops@'+node[:domain]}
+default['god']['init_style']        = 'runit'
+default['god']['email']['from']     = 'god@'+node[:domain].to_s
+default['god']['email']['contacts'] = {'developers' => 'developers@'+node[:domain].to_s}
+
+default['god']['email']['server']['delivery_method'] = :smtp
+default['god']['email']['server']['server_host'] = 'localhost'
+default['god']['email']['server']['server_port'] = 25
+default['god']['email']['server']['server_auth'] = false
+default['god']['email']['server']['server_domain']   = ''
+default['god']['email']['server']['server_user']     = ''
+default['god']['email']['server']['server_password'] = ''
