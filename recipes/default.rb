265fabeed2bc7d587014291666a54d679c3b4a1a
Josh Moore <joshsmoore@gmail.com>
2015-03-31 09:51:44 +0800

    updates
    * updated master file to work with new version of god
    * switched from runit to init.d to make god work with RVM

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,6 @@
 
 gem_package "god" do
   action :install
-  gem_binary "/usr/bin/gem"
 end
 
 directory "/etc/god/conf.d" do
@@ -36,18 +35,15 @@ template "/etc/god/master.god" do
   mode 0755
 end
 
-if node['god']['init_style'] == 'runit'
-  include_recipe "runit"
-  runit_service "god"
-elsif node['god']['init_style'] == 'init'
-  template "/etc/init.d/god" do
-    source "god.init.erb"
-    owner "root"
-    group "root"
-    mode 0755
-  end
-
-  service "god" do
-    action :start
-  end
+template "/etc/init.d/god" do
+  source "god.erb"
+  owner "root"
+  group "root"
+  mode 0755
 end
+
+service 'god' do
+  supports reload: false
+  action [:start, :enable]
+end
+
